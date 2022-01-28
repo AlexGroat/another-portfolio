d3738294f553d96176f36f2c7e9db1a3501c6709
Alex Groat <alex.groat.95@gmail.com>
2022-01-28 16:57:57 +1030

    Update

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import styled, { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme, GlobalStyles } from "./themes.js";
 import Intro from "./components/intro/intro";
 
+
 const StyledApp = styled.div`
   color: ${(props) => props.theme.fontColor};
 `;
-
-
 
 function App() {
   const [theme, setTheme] = useState("light");
@@ -21,7 +20,7 @@ function App() {
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
       <GlobalStyles />
       <StyledApp>
-        <button id="myButton" onClick={() => themeToggler()}>Lights Off</button>
+        <button className="myButton" onClick={() => themeToggler()}>{theme === 'light' ? 'Lights Off' : 'Lights On'}</button>
         <Intro />
       </StyledApp>
     </ThemeProvider>
--- src/themes.js
+++ src/themes.js
@@ -3,16 +3,22 @@ import { createGlobalStyle } from "styled-components";
 export const lightTheme = {
   body: "#fff",
   fontColor: "#000",
+  btnBackground: "#eee",
   
 };
 
 export const darkTheme = {
   body: "#121212",
   fontColor: "#fff",
+  btnBackground: "#040404",
 };
 
 export const GlobalStyles = createGlobalStyle`
 	body {
 		background-color: ${(props) => props.theme.body};
 	}
+  button {
+    color: ${({theme}) => theme.fontColor};
+    background-color: ${({theme}) => theme.btnBackground};
+  }
 `;
